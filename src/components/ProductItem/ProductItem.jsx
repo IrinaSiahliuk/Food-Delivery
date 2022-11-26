cf4873f1ce092a9ac55d9741358084673c67dc3e
4elovekD <slava04.06.98@mail.ru>
2022-11-26 20:50:23 +0300

    feat(FE-2): add component productItem

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,10 +1,29 @@
+import React from "react";
 
+import ItemCSS from "./ProductItem.module.css";
 
-import React from 'react';
-
-function ProductItem() {
+function ProductItem({ image, title, text, price }) {
   return (
-    <div>ProductItem</div>
+    <div className={ItemCSS.item_wrapper}>
+      <div className={ItemCSS.img_wrapper}>
+        <img className={ItemCSS.pizza_img} src={image} alt="" />
+      </div>
+
+      <div className={ItemCSS.product_description}>
+        <div className={ItemCSS.description_title}>
+          <h2 className={ItemCSS.title}>{title}</h2>
+        </div>
+
+        <div className={ItemCSS.text_wrapper}>
+          <p className={ItemCSS.text}>{text}</p>
+        </div>
+      </div>
+
+      <div className={ItemCSS.buy_area}>
+        <span className={ItemCSS.price}> от {price} ₽</span>
+        <button className={ItemCSS.basket_btn}>В корзину</button>
+      </div>
+    </div>
   );
 }
 
--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
-import ProductItem from '../../components/ProductItem/ProductItem';
+import React from "react";
+
+import MenuCSS from './Menu.module.css'
+
+import ProductItem from "../../components/ProductItem/ProductItem";
+
+import FourChease from "../../components/ProductItem/Assets/PizzasImg/four-chease.png";
+import FourSeasonsPizza from "../../components/ProductItem/Assets/PizzasImg/four-seasons.png";
+import KetchupPizza from "../../components/ProductItem/Assets/PizzasImg/ketchup.png";
+import MushroomPizza from "../../components/ProductItem/Assets/PizzasImg/mushroom.png";
 
 function Menu() {
   return (
-    <div>
+    <div className={MenuCSS.menu_wrapper}>
       Menu
-      <ProductItem/>
-      <ProductItem/>
-      <ProductItem/>
-      <ProductItem/>
-      <ProductItem/>
+      <ProductItem
+        image={FourChease}
+        title="Четыре сыра"
+        text="Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан.350 г"
+        price="600"
+      />
+      <ProductItem
+        image={FourSeasonsPizza}
+        title="Четыре сезона"
+        text="Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан.350 г"
+        price="750"
+      />
+      <ProductItem
+        image={KetchupPizza}
+        title="Пицца с кетчупом"
+        text="Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан.350 г"
+        price="650"
+      />
+      <ProductItem
+        image={MushroomPizza}
+        title="Грибная пицца"
+        text="Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан.350 г"
+        price="700"
+      />
     </div>
   );
 }
