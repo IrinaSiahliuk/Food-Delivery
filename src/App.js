3e72262cf0accfc3ffc115b8b7ba1741f547c292
IrinaSiahliuk <ira.segluk15@gmail.com>
2022-11-26 12:27:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+
 import './App.css';
+import Home from './pages/Home/Home';
+import AboutUs from './pages/AboutUs/AboutUs';
+import Menu from './pages/Menu/Menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<AboutUs />} />
+        <Route path='/menu' element={<Menu />} />
+      </Routes>
     </div>
   );
 }
