0b0a1275ebd8e3300e623752cecfcf75be0b023a
Vadim_B <bachkovvadim@gmail.com>
2022-11-28 15:55:50 +0300

    Added corrections

--- src/components/DiscountItem/DiscountItem.jsx
+++ src/components/DiscountItem/DiscountItem.jsx
@@ -3,20 +3,19 @@ import React from "react";
 import style from "./styles.module.css";
 
 
-function DiscountItem({image,title,description,button}){
+function DiscountItem({ image, title, description }) {
 
-    return(
+    return (
         <div className={style.wrapper}>
             <div className={style.item}>
                 <div className={style.img}>
                     <img src={image} alt="Discount" />
                 </div>
+                <div className={style.title}>{title}</div>
                 <div className={style.info}>
-                    <div className={style.title}>{title}</div>
                     <div className={style.text}>{description}</div>
-                    <button className={style.button}>{button}</button>
                 </div>
-
+                <button className={style.button}>Посмотреть</button>
             </div>
 
         </div>
