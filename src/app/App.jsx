fbb0f197aea762066da2c6b2af551e7344d1b46d
RomaZherko21 <RomaZherko21@inbox.ru>
2022-12-15 21:27:50 +0300

    feat(FE-0): add store

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import "./App.css";
 import { Layout } from "./Layout";
 import Router from "./Router";
 
+export const Store = createContext();
+
 function App() {
+  const [store, setStore] = useState({
+    name: "keke",
+    counter: 0,
+  });
+
   return (
     <React.StrictMode>
       <BrowserRouter>
-        <Layout>
-          <Router />
-        </Layout>
+        <Store.Provider value={[store, setStore]}>
+          <>
+            <Layout>
+              <Router />
+            </Layout>
+          </>
+        </Store.Provider>
       </BrowserRouter>
     </React.StrictMode>
   );
