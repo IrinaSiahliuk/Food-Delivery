2843db8a21737cf9091478b7f78e378fb16035e0
Vadim_B <bachkovvadim@gmail.com>
2022-12-26 19:23:21 +0300

    feat(FE-12): add product modal

--- src/components/ModalOrderForm/ModalOrderForm.jsx
+++ src/components/ModalOrderForm/ModalOrderForm.jsx
@@ -1,8 +1,9 @@
-import React,{useState} from "react";
+import React, {useContext} from "react";
 
 import style from "components/ModalOrderForm/styles.module.css";
 
-import {SauceItem} from "components";
+import { SauceItem } from "components";
+import { Store } from "app";
 
 import FourChease from "assets/PizzasImg/four-chease.png";
 import FourSeasonsPizza from "assets/PizzasImg//four-seasons.png";
@@ -12,12 +13,21 @@ import RanchSauce from "assets/SaucesImg/RanchSauce.png";
 import CheeseSauce from "assets/SaucesImg/CheeseSauce.png";
 import RaspberryJam from "assets/SaucesImg/RaspberryJam.png";
 
-function ModalOrderForm ({image,title,description,price}){
 
-    return(
-            <div className={style.wrapper}>
+function ModalOrderForm({ image, title, description, isVisible = false, onClose, props }) {
+        const [store, setStore] = useContext(Store);
+        const onAddProduct = () => {
+            setStore((pre) => ({
+              ...pre,
+              shoppingCart: [...pre.shoppingCart, { ...props }],
+            }));
+          };
+
+    return !isVisible ? null : (
+        <div className={style.modal} onClick={onClose}>
+            <div className={style.wrapper} onClick={e => e.stopPropagation()}>
                 <div className={style.imgWrapper}>
-                    <img className={style.img} src={image} alt="" />
+                    <img className={style.img} src={image} alt=""/>
                 </div>
                 <div className={style.selectWrapper}>
                     <div className={style.title}>{title}</div>
@@ -32,38 +42,28 @@ function ModalOrderForm ({image,title,description,price}){
                         <button className={style.doughBtn}>Тонкое тесто</button>
                     </div>
                     <div className={style.sauces}>
-                    <SauceItem
-                        image={CheeseSauce}
-                        title="Сырный соус"
-                        price={0.6}
-                    />
-                    <SauceItem
-                        image={BarbecueSauce}
-                        title="Бербукю"
-                        price={0.6}
-                    />
-                    <SauceItem
-                        image={RanchSauce}
-                        title="Ранч"
-                        price={0.6}
-                    />
-                    <SauceItem
-                        image={RaspberryJam}
-                        title="Сгущенка"
-                        price={0.6}
-                    />
-                    <SauceItem
-                        image={CondensedMilk}
-                        title="Малиновое варенье"
-                        price={0.6}
-                    />
+                        <SauceItem
+                            image={CheeseSauce}
+                            title="Сырный соус"
+                            price={0.6}
+                        />
+                        <SauceItem
+                            image={BarbecueSauce}
+                            title="Бербукю"
+                            price={0.6}
+                        />
+                        <SauceItem
+                            image={RanchSauce}
+                            title="Ранч"
+                            price={0.6}
+                        />
                     </div>
-                    <div className={style.orederBtnWrapper}>
-                        <button className={style.orderBtn}>Добавить в корзину</button>
+                    <div className={style.orederBtnWrapper} onClick={onAddProduct}>
+                        <button className={style.orderBtn} onClick={onClose}>Добавить в корзину</button>
                     </div>
                 </div>
-
             </div>
+        </div>
     )
 }
 
--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,44 +1,45 @@
-import React, { useContext } from "react";
+import React, { useState } from "react";
 
 import styles from "components/ProductItem/styles.module.css";
-import { Store } from "app";
+
+import { ModalOrderForm } from "components";
 
 function ProductItem(props) {
   const { image, title, text, price } = props;
-  const [store, setStore] = useContext(Store);
-
-  const onAddProduct = () => {
-    setStore((pre) => ({
-      ...pre,
-      user: {
-        ...pre.user, 
-        shoppingCart: [...pre.user.shoppingCart, {...props }]}
-    }));
-  };
+  const [isModal, setModal] = useState(false);
 
   return (
-    <div className={styles.item_wrapper}>
-      <div className={styles.img_wrapper}>
-        <img className={styles.pizza_img} src={image} alt="" />
-      </div>
-
-      <div className={styles.product_description}>
-        <div className={styles.description_title}>
-          <h2 className={styles.title}>{title}</h2>
+    <>
+      <div className={styles.item_wrapper}>
+        <div className={styles.img_wrapper}>
+          <img className={styles.pizza_img} src={image} alt="" />
         </div>
 
-        <div className={styles.text_wrapper}>
-          <p className={styles.text}>{text}</p>
+        <div className={styles.product_description}>
+          <div className={styles.description_title}>
+            <h2 className={styles.title}>{title}</h2>
+          </div>
+
+          <div className={styles.text_wrapper}>
+            <p className={styles.text}>{text}</p>
+          </div>
+        </div>
+
+        <div className={styles.buy_area}>
+          <span className={styles.price}> от {price} ₽</span>
+          <button className={styles.basket_btn} onClick={() => setModal(true)}>
+            Выбрать
+          </button>
         </div>
       </div>
-
-      <div className={styles.buy_area}>
-        <span className={styles.price}> от {price} ₽</span>
-        <button className={styles.basket_btn} onClick={onAddProduct}>
-          В корзину
-        </button>
-      </div>
-    </div>
+      <ModalOrderForm
+        isVisible={isModal}
+        image={image}
+        title={title}
+        description={text}
+        onClose={() => setModal(false)}
+      />
+    </>
   );
 }
 
